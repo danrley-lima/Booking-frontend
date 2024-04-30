3b7563740063eab73fd12b92eda83ca43f759fc2
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-04-30 10:58:50 -0300

    feat: added icons and made adjustments to CSS

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaFacebook, FaInstagram, FaTiktok, FaTwitter } from 'react-icons/fa';
+
 function Footer() {
     return (
         <footer className="h-45 bg-azul px-4 text-cinza sm:px-8 md:px-12 lg:px-16 flex fixed bottom-0 left-0 w-full">
@@ -36,7 +38,13 @@ function Footer() {
                 </ul>
             </section>
             <section className="py-4 text-left text-sm sm:px-10 md:px-12 lg:py-12 lg:w-1/3">
-                <h2 className="font-bold">Siga-nos</h2>
+                <div className="flex items-center mb-14">
+                    <h2 className="font-bold mr-3">Siga-nos</h2>
+                    <FaFacebook className="text-white-900 w-4 h-4 mr-4 hover:text-blue-500" />
+                    <FaInstagram className="text-white-900 w-4 h-4 mr-4 hover:text-purple-500" />
+                    <FaTiktok className="text-white-900 w-4 h-4 mr-4 hover:text-pink-500" />
+                    <FaTwitter className="text-white-900 w-4 h-4 hover:text-blue-400" />
+                </div>
 
                 <p>© 2024 Booking da Shopee. Todos os direitos reservados.</p>
             </section>

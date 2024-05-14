1a0389faebb22c876f3e88429f6a3a6899291056
Lucas Pires <lucaspires2807@gmail.com>
2024-05-14 09:40:54 -0300

    feature: responsive ModalLogin

--- src/app/components/ModalLogin.tsx
+++ src/app/components/ModalLogin.tsx
@@ -6,30 +6,30 @@ import { IoClose } from "react-icons/io5";
 function ModalLogin(){
     return(
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50 backdrop-blur-md text-preto bg-white">
-            <div className="bg-cinza p-8 rounded-md shadow-md h-5/6 w-4/12 relative">
+            <div className="bg-cinza p-8 rounded-md shadow-md relative h-5/6 w-screen sm:w-1/2 md:1/3">
                 <button className="absolute right-3 top-3">
                     <IoClose/>
                 </button>
-                <p className="text-2xl text-align pt-16 px-10 font-bold inline-block">
+                <p className="text-align font-bold inline-block text-xs px-0 pt-8 sm:text-sm md:text-base md:pt-16 md:px-10">
                     Entre para ver o melhor do Booking da Shopee.
                 </p>
                 <div className="pt-12">
-                    <button className="text-base relative border-2 rounded-3xl w-full h-10 mb-3 border-preto">
+                    <button className="relative border-2 rounded-3xl w-full h-10 mb-3 border-preto text-xs sm:text-sm md:text-base">
 
-                        <FcGoogle className="absolute left-3 size-6"/>
+                        <FcGoogle className="absolute left-3 size-6 hidden sm:inline-block"/>
                         
                         Continuar com o Google
 
                     </button>
-                    <button className="text-base relative border-2 rounded-3xl w-full h-10 mb-4 border-preto">
+                    <button className="relative border-2 rounded-3xl w-full h-10 mb-4 border-preto text-xs sm:text-sm md:text-base">
 
-                        <MdOutlineEmail className="absolute left-3 size-6"/>
+                        <MdOutlineEmail className="absolute left-3 size-6 hidden sm:inline-block"/>
 
                         Continuar com o e-mail
 
                     </button>
                 </div>
-                <div className="flex flex-col items-center pt-2 text-base font-bold">
+                <div className="flex flex-col items-center pt-2 font-bold text-xs sm:text-sm md:text-base">
                     <button className="border-2 rounded-md bg-azul text-cinza mb-1 bottom-2 w-11/12">
                         Login
                     </button>
@@ -37,7 +37,7 @@ function ModalLogin(){
                         Cadastre-se
                     </button>
                 </div>
-                <div className="pt-4 text-center text-xs w-full">
+                <div className="text-center w-full text-xs pt-1 sm:text-sm md:pt-4">
                     <p className="mb-2">
                         Ao continuar, você concorda com os Termos de uso e confirma que leu nossa Política de privacidade e cookies.
                     </p>

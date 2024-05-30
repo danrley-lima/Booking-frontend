6f41a0288fb0d0e20de9b0ea399eb226e6bec8e2
Lucas Pires <lucaspires2807@gmail.com>
2024-05-30 12:43:35 -0300

    feature: user interface

---
+++ src/app/utils/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    name: string;
+    lastName: string;
+    password: string;
+    email: string;
+    telephone: string;
+    cpf: string,
+    gender: string;
+    birthDate: string;
+    nationality: string;
+}

3af3dc924c22752eecc935b4f9124c0926369df7
Danrley Lima <danrleylac@gmail.com>
2024-06-24 19:00:56 -0300

    feat: add login and registration modals to busca layout

--- src/app/busca/layout.tsx
+++ src/app/busca/layout.tsx
@@ -1,15 +1,39 @@
+"use client";
+
+import { useState } from "react";
 import Header from "../components/Header";
+import ModalCadastro from "../components/ModalCadastro";
+import ModalLogin from "../components/ModalLogin";
 import SmallFooter from "../components/SmallFooter";
-import SmallHeader from "../components/SmallHeader";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [openLogin, setOpenLogin] = useState(false);
+  const [openRegister, setOpenRegister] = useState(false);
+  const [openPage, setOpenPage] = useState("");
+
+  function openModalLogin() {
+    setOpenLogin(!openLogin);
+  }
+
+  function openModalRegister() {
+    setOpenRegister(!openRegister);
+  }
   return (
     <div className="flex min-h-screen flex-col justify-between">
-      <Header />
+      {openLogin ? (
+        <ModalLogin
+          closeModal={openModalLogin}
+          openRegister={openModalRegister}
+        />
+      ) : (
+        <></>
+      )}
+      {openRegister ? <ModalCadastro closeModal={openModalRegister} /> : <></>}
+      <Header openModalLogin={openModalLogin} setPage={setOpenPage} />
       <div className="flex-grow bg-cinza">{children}</div>
       <SmallFooter />
     </div>
